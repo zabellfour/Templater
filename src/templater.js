51a8c95cdcd281cf6a98eb81de84cb4ee1f88e08
Andrew Davydenko <adavydenko@justcoded.co>
2018-05-10 13:09:13 +0300

    stage3 done

--- src/templater.js
+++ src/templater.js
@@ -1,11 +1,26 @@
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
 let Templater = {
     run: function() {
+
         for (i in this.newTags) {
             let pageEl = Array.from(document.getElementsByTagName(i));
-            let newEl = document.createElement('div');
-            newEl.innerHTML = this.newTags[i];
             pageEl.forEach(element => {
-                element.replaceWith(newEl.firstChild)
+                let classList = element.classList + '';
+                let typeAttr = element.getAttribute('type');
+                let content = element.innerHTML;
+                let parentEl = element.parentElement;
+                let newEl = this.newTags[i]
+                    .replace('{{class}}', classList)
+                    .replace('{{type}}', typeAttr)
+                    .replace('{{html}}', content);
+                parentEl.insertBefore(htmlToElement(newEl), element);
+                element.remove();
             });
         }
     },
@@ -17,7 +32,17 @@ let Templater = {
     }
 }
 
-// Templater.addTag('bootstrap_button', '<button class="btn btn-default" type = "submit"> Some Text </button>');
-// Templater.addTag('bootstrap_link', '<a class="btn btn-default" href="#" role="button">Some Another Text</a>');
+function getAttr(el) {
+    let elClass = el.classList;
+    return elClass;
+}
+
+// Templater.addTag(
+//     'bootstrap_button',
+//     '<button class="{{class}}" type="{{type}}">' +
+//     '{{html}}' +
+//     '</button>'
+// );
+
 // Templater.run();
 //console.log(Templater.newTags);
